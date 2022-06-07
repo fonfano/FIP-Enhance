2a9000c585cf327bc73f170e16b0d6735aabfd8f
fonfano <fonfano@gmail.com>
2022-06-07 21:16:02 +0200

    Update FipEnhancer.user.js

--- FipEnhancer.user.js
+++ FipEnhancer.user.js
@@ -3,23 +3,31 @@
 // @namespace   github.com/fonfano
 // @match       https://www.radiofrance.fr/*
 // @grant       none
-// @version     0.4.1
+// @version     0.5
 // @author      Lt Ripley
 // @description 07/01/2022, 11:33:14
 // ==/UserScript==
 
-// Remove uggly play buttons
+// Remove uggly play buttons, raise lower FIP radios sections
 
 // Historique
-// 05/05/2022   0.4.1  Correction : Adaptation au nouveau nom des boutons
-// 05/04/2022   0.4    Upgrade  Pour nouveau site FIP
-// 05/02/2022   0.3    Upgrade  Remove all uggly play buttons
-// 25/01/2022   0.2    Upgrade  Remove uggly play button on art
-// 07/01/2022   0.1    Création
+// 07/06/2022   0.5     Upgrade :  Raise lower FIP Radios sections
+// 05/05/2022   0.4.1   Fix : For new buttons name
+// 05/04/2022   0.4     Upgrade :  For new fip site (including URL)
+// 05/02/2022   0.3     Upgrade :  Remove all uggly play buttons
+// 25/01/2022   0.2     Upgrade :  Remove uggly play button on art
+// 07/01/2022   0.1     Creation
+
+
+// Options
+let delay = 1500;              // Time (in MS) before the script runs (wait the page to be fully loaded).  Increase if necessary.
+let raiseFipSections = true;   // Raise a little bit the lower FIP radios sections, to be able to read the text, especialy in case of MS Windows 125% display scale
+let height = "480px";          // Height (decrease to raise) 
+// End of options
 
 
 setTimeout(() => {     
-         
+     
   document.querySelector("body > div:nth-child(3) > main > section.Home-webradios.g-block-margin.svelte-19wlv8z.dark.isImmersive > div > div > div > div > div.Carousel-container.svelte-qnall9 > div > div:nth-child(1) > div > div > div > div.CardWebRadio-playButton.svelte-msxzte").style.display = "none";
   document.querySelector("body > div:nth-child(3) > main > section.Home-webradios.g-block-margin.svelte-19wlv8z.dark.isImmersive > div > div > div > div > div.Carousel-container.svelte-qnall9 > div > div:nth-child(2) > div > div > div > div.CardWebRadio-playButton.svelte-msxzte").style.display = "none";
   document.querySelector("body > div:nth-child(3) > main > section.Home-webradios.g-block-margin.svelte-19wlv8z.dark.isImmersive > div > div > div > div > div.Carousel-container.svelte-qnall9 > div > div:nth-child(3) > div > div > div > div.CardWebRadio-playButton.svelte-msxzte").style.display = "none";
@@ -29,4 +37,7 @@ setTimeout(() => {
   document.querySelector("body > div:nth-child(3) > main > section.Home-webradios.g-block-margin.svelte-19wlv8z.dark.isImmersive > div > div > div > div > div.Carousel-container.svelte-qnall9 > div > div:nth-child(7) > div > div > div > div.CardWebRadio-playButton.svelte-msxzte").style.display = "none";
   document.querySelector("body > div:nth-child(3) > main > section.Home-webradios.g-block-margin.svelte-19wlv8z.dark.isImmersive > div > div > div > div > div.Carousel-container.svelte-qnall9 > div > div:nth-child(8) > div > div > div > div.CardWebRadio-playButton.svelte-msxzte").style.display = "none";
   
-}, 1000);
+  if (raiseFipSections)  { document.querySelector("body > div:nth-child(3) > main > section.Home-coverRadio").style.maxHeight = height; 
+  }             
+  
+}, delay);
