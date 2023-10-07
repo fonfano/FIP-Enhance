21c09901431f666d15222350602b9ccd2f91f33e
fonfano <fonfano@gmail.com>
2023-10-07 09:02:26 +0200

    Update FipEnhancer.user.js

--- FipEnhancer.user.js
+++ FipEnhancer.user.js
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 // Historique
-// 07-10-2023   0.9.3   Update  :  For new buttons names
+// 07-10-2023   0.9.3   Update  :  For new buttons names and delete new animation on played radio
 // 29-08-2023   0.9.2   Update  :  New radios order
 // 28-08-2023   0.9.1   Update  :  Added new radio (Sacré Français).
 // 09-08-2023   0.9.0   Update  :  Added blinking mode for border, with option.
@@ -83,10 +83,14 @@ window.addEventListener("load", (event) => {  // Censé attendre que la page soi
 function removePlayButtons() {
 
   const playButtons = document.querySelectorAll('.WebradioButton-remote-state.svelte-18lspnn')
-  
   for (const button of playButtons) {
   //button.style.display = "none";
     button.remove();
+  }
+  
+  const playGraph = document.querySelectorAll('.WebradioButton-overlay-background.svelte-18lspnn')
+  for (const graph of playGraph) {
+    graph.remove();
   }
 }
 
